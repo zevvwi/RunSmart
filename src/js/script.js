19e8d47c5dd33eed54c1a0a33c0663db333c1531
zeywi <zeywi@mail.ru>
2022-09-17 00:16:01 +0700

    Added css animation, php mailer, pageup button

--- src/js/script.js
+++ src/js/script.js
@@ -87,4 +87,36 @@ $(document).ready(function(){
     validateForm('#order form');
 
     $('input[name=phone]').mask("+7(999) 999-99-99");
+
+    $('form').submit(function(e) {
+      e.preventDefault();
+      $.ajax({
+        type: "POST",
+        url: "../mailer/smart.php",
+        data: $(this).serialize()
+      }).done(function() {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #success').fadeIn('slow');
+
+        $('form').trigger('reset');
+      });
+      return false;
+    });
+
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+      } else {
+        $('.pageup').fadeOut();
+      }
+    });
+
+    $("a[href=#up]").click(function() {
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+    });
+
+    new WOW().init();
 });
